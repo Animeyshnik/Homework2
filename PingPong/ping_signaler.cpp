59137b11e8bf1c8e464036c03ada19176400c452
Animeyshnik <davidazizzyan@gmail.com>
2025-03-12 14:27:51 -0400

    PingPong_Homework_2.2

---
+++ PingPong/ping_signaler.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <cstring>
+#include <unistd.h>
+
+struct message {
+    long type;
+    char text[10];
+};
+
+int main() {
+    key_t key = ftok("msgqueue", 65);
+    int msgid = msgget(key, 0666 | IPC_CREAT);
+
+    message msg;
+    while (true) {
+        strcpy(msg.text, "PING");
+        msg.type = 1;
+        msgsnd(msgid, &msg, sizeof(msg.text), 0);
+        std::cout << "Sent: " << msg.text << std::endl;
+
+        msgrcv(msgid, &msg, sizeof(msg.text), 2, 0);
+        std::cout << "Received: " << msg.text << std::endl;
+        sleep(1);
+    }
+
+    return 0;
+}
+
