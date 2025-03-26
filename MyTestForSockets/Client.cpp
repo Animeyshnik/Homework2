0caec09fd2ec04cb8a32b4089c3124cade4dbdd3
Animeyshnik <davidazizzyan@gmail.com>
2025-03-26 14:15:14 -0400

    sockets_with_pthread

--- MyTestForSockets/Client.cpp
+++ MyTestForSockets/Client.cpp
@@ -3,40 +3,68 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <thread>
 
-#define SERVER_IP "127.0.0.1"  
-#define PORT 8080              
+#define PORT 8080
+
+int client_fd;
+
+void receive_messages() {
+    char buffer[1024];
+    while (true) {
+        memset(buffer, 0, sizeof(buffer));
+        int bytes_received = recv(client_fd, buffer, sizeof(buffer) - 1, 0);
+        if (bytes_received > 0) {
+            std::cout << "Message from another client: " << buffer << std::endl;
+        } else if (bytes_received == 0) {
+            std::cout << "Server closed the connection.\n";
+            break;
+        } else {
+            std::cerr << "Failed to receive message\n";
+            break;
+        }
+    }
+}
 
 int main() {
-    int client_fd = socket(AF_INET, SOCK_STREAM, 0);
+    client_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (client_fd == -1) {
-        std::cerr << "Socket creation failed" << std::endl;
+        std::cerr << "Socket creation failed\n";
         return -1;
     }
 
-    struct sockaddr_in server_addr;
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(PORT);
-    server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
+    struct sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server_address.sin_port = htons(PORT);
 
-    if (connect(client_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
-        std::cerr << "Connection failed" << std::endl;
+    if (connect(client_fd, (struct sockaddr*)&server_address, sizeof(server_address)) < 0) {
+        std::cerr << "Connection to server failed\n";
         close(client_fd);
         return -1;
     }
 
-    std::cout << "Connected to server. Type messages to send:\n";
+    std::cout << "Connected to server. You can start typing your messages.\n";
 
-    std::string input;
+    std::thread(receive_messages).detach();
+
+    char buffer[1024];
     while (true) {
-        std::cout << "> ";
-        std::getline(std::cin, input);
-        if (input == "exit") break;
-        send(client_fd, input.c_str(), input.length(), 0);
+        memset(buffer, 0, sizeof(buffer));
+        std::cout << "Enter message: ";
+        std::cin.getline(buffer, sizeof(buffer));
+
+        if (strcmp(buffer, "exit") == 0) {
+            std::cout << "Exiting...\n";
+            break;
+        }
+
+        if (send(client_fd, buffer, strlen(buffer), 0) == -1) {
+            std::cerr << "Failed to send message to server\n";
+        }
     }
 
     close(client_fd);
     return 0;
 }
-
 
--- MyTestForSockets/Server.cpp
+++ MyTestForSockets/Server.cpp
@@ -3,13 +3,51 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <thread>
+#include <vector>
+#include <mutex>
+#include <algorithm>
 
-#define PORT 8080  
+#define PORT 8080
+
+std::vector<int> clients;
+std::mutex clients_mutex;
+
+void handle_client(int client_fd) {
+    char buffer[1024];
+
+    while (true) {
+        memset(buffer, 0, sizeof(buffer));
+        int bytes_received = recv(client_fd, buffer, sizeof(buffer) - 1, 0);
+        if (bytes_received <= 0) {
+            std::cout << "Client disconnected.\n";
+            close(client_fd);
+
+            clients_mutex.lock();
+            clients.erase(std::remove(clients.begin(), clients.end(), client_fd), clients.end());
+            clients_mutex.unlock();
+
+            break;
+        }
+
+        std::cout << "Received message from client: " << buffer << std::endl;
+
+        clients_mutex.lock();
+        for (int c : clients) {
+            if (c != client_fd) { 
+    		    if (send(c, buffer, bytes_received, 0) == -1) {
+                    std::cerr << "Failed to send message to client " << c << std::endl;
+                }
+            }
+        }
+        clients_mutex.unlock();
+    }
+}
 
 int main() {
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_fd == -1) {
-        std::cerr << "Socket creation failed" << std::endl;
+        std::cerr << "Socket creation failed\n";
         return -1;
     }
 
@@ -19,40 +57,35 @@ int main() {
     address.sin_port = htons(PORT);
 
     if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
-        std::cerr << "Bind failed" << std::endl;
+        std::cerr << "Bind failed\n";
         close(server_fd);
         return -1;
     }
 
-    if (listen(server_fd, 3) < 0) {
-        std::cerr << "Listen failed" << std::endl;
+    if (listen(server_fd, 5) < 0) {
+        std::cerr << "Listen failed\n";
         close(server_fd);
         return -1;
     }
 
-    std::cout << "Waiting for connections..." << std::endl;
+    std::cout << "Server is waiting for connections...\n";
 
-    int client_fd = accept(server_fd, nullptr, nullptr);
-    if (client_fd < 0) {
-        std::cerr << "Accept failed" << std::endl;
-        close(server_fd);
-        return -1;
-    }
-
-    std::cout << "Client connected! Waiting for messages...\n";
-
-    char buffer[1024] = {0};
     while (true) {
-        memset(buffer, 0, sizeof(buffer));
-        int bytes_received = recv(client_fd, buffer, sizeof(buffer) - 1, 0);
-        if (bytes_received <= 0) {
-            std::cout << "Client disconnected.\n";
-            break;
+        int client_fd = accept(server_fd, nullptr, nullptr);
+        if (client_fd < 0) {
+            std::cerr << "Accept failed\n";
+            continue;
         }
-        std::cout << "Client: " << buffer << std::endl;
+
+        clients_mutex.lock();
+        clients.push_back(client_fd);
+        clients_mutex.unlock();
+
+        std::cout << "New client connected!\n";
+
+        std::thread(handle_client, client_fd).detach();  
     }
 
-    close(client_fd);
     close(server_fd);
     return 0;
 }
